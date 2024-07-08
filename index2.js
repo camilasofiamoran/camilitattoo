24775dd89be0bcb2a778615865ee74d7056b0615
camilasofiamoran <ckamila.tattoo@gmail.com>
2024-07-08 13:52:31 +0200

    bala

--- index2.js
+++ index2.js
@@ -1,5 +1,6 @@
-
+var citaForm=[]
 var actual = 0;
+var fotos=[]
 
 var fondos =[
     "uno.png","dos.png", "tres.png","cuatro.png","cinco.png","seis.png",
@@ -15,3 +16,28 @@ function cambioFondo()
 }
 setInterval (cambioFondo,2000)
 cambioFondo ()
+
+  function enviarFormularioCita() {
+  let nombre=document.getElementById("nombre").value 
+  let telefono=document.getElementById("telefono").value
+  let mail=document.getElementById("mail").value
+
+if(nombre && telefono && mail) {
+let objeto={nombre,telefono,mail}
+citaForm.push(objeto) 
+  }
+  console.log(citaForm)
+  } 
+  
+  
+  function enviarFormularioFoto(){
+let fotito1=document.getElementById("fotito1")
+let fotito2=document.getElementById("fotito2")
+if (fotito1 && fotito2){
+    
+    fotos.push(fotito1)
+    fotos.push(fotito2)
+
+}
+console.log(fotos)
+  }
